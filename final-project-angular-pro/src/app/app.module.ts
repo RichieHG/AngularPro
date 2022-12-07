1c8e5ebf0f13d81579405bf3d982129c064249e0
Ricardo Neftali Hernandez Gomez <neftali.hergom@gmail.com>
2022-12-06 23:17:50 -0600

    Feat: Add init firebase

--- final-project-angular-pro/src/app/app.module.ts
+++ final-project-angular-pro/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Route, RouterModule } from '@angular/router';
+import { Store } from 'src/store';
+import { AppComponent } from './containers/app/app.component';
 
-import { AppComponent } from './app.component';
 
+const ROUTES: Route[] = [];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [
+    Store
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
