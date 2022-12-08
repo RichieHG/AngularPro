46e2d4f6db430f152cb51833857df94cb3db7129
Ricardo Neftali Hernandez Gomez <neftali.hergom@gmail.com>
2022-12-07 22:02:12 -0600

    Feat: Add ReactiveStore

--- final-project-angular-pro/src/app/containers/app/app.component.ts
+++ final-project-angular-pro/src/app/containers/app/app.component.ts
@@ -1,18 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { AuthService, User } from 'src/app/auth/shared/services/auth/auth.service';
+import { Store } from 'src/store';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div>
-      Hello Ultimate Angular!
+      <h1>{{user$ | async | json}}</h1>
       <div class="wrapper">
         <router-outlet></router-outlet>
       </div>
     </div>
   `
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy{
+
+  user$?: Observable<User>;
+  subscription?: Subscription;
+  constructor(
+    private store: Store,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.subscription = this.authService.auth$.subscribe();
+    this.user$ =this.store.select('user');
+  }
+
+  ngOnDestroy() {
+      this.subscription?.unsubscribe();
+  }
 }
 
--- final-project-angular-pro/src/store.ts
+++ final-project-angular-pro/src/store.ts
@@ -1,10 +1,14 @@
 import { BehaviorSubject, distinctUntilChanged, map, Observable } from "rxjs";
+import { User } from "./app/auth/shared/services/auth/auth.service";
 
 export interface State {
+  user: User | undefined,
   [key: string]: any
 }
 
-const state: State = {};
+const state: State = {
+  user: undefined
+};
 
 export class Store {
 
