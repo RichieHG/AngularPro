45545e1d28fc45658a44dadee0fbeb024240748b
Ricardo Neftali Hernandez Gomez <neftali.hergom@gmail.com>
2022-12-12 17:08:38 -0600

    Feat: Finish Meal CRUD and add example about async/await into reactiveForms

--- final-project-angular-pro/src/health/meals/containers/meal/meal.component.ts
+++ final-project-angular-pro/src/health/meals/containers/meal/meal.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, Subscription, switchMap } from 'rxjs';
 import { Meal, MealsService } from 'src/health/shared/services/meals.service';
 
 @Component({
@@ -10,20 +11,35 @@ import { Meal, MealsService } from 'src/health/shared/services/meals.service';
         <div class="meal__title">
             <h1>
                 <img src="/assets/img/food.svg">
-                <span>Create meal</span>
+                <span *ngIf="meal$ | async as meal; else title;">
+                    {{meal.name ? 'Edit' : 'Create'}} meal
+                </span>
+                <ng-template #title>
+                    Loading...
+                </ng-template>
             </h1>
         </div>
-        <div>
-            <meal-form (create)="addMeal($event)"></meal-form>
+        <div *ngIf="meal$ | async as meal; else loading;">
+            <meal-form (create)="addMeal($event)" (update)="updateMeal($event)" (remove)="removeMeal($event)"
+            [meal]="meal"></meal-form>
         </div>
+        <ng-template #loading>
+            <div class="message">
+                <img src="/assets/img/loading.svg">
+                Fetching meal...
+            </div>
+        </ng-template>
     </div>
     `
 })
 
-export class MealComponent   {
+export class MealComponent implements OnInit, OnDestroy  {
+    meal$?: Observable<Meal | undefined>;
+    subscription?: Subscription;
     constructor(
         private mealService: MealsService,
-        private router: Router
+        private router: Router,
+        private route: ActivatedRoute
     ) { }
 
     async addMeal(event: Meal){
@@ -31,8 +47,29 @@ export class MealComponent   {
         this.backToMeals();
     }
 
+    async updateMeal(event: Meal){
+        const key = this.route.snapshot.params['id'];
+        await this.mealService.updateMeal(key,event);
+        this.backToMeals();
+    }
+    async removeMeal(event: Meal){
+        const key = this.route.snapshot.params['id'];
+        await this.mealService.removeMeal(key);
+        this.backToMeals();
+    }
     backToMeals(){
         this.router.navigate(['meals'])
     }
 
+    ngOnInit() {
+        this.subscription = this.mealService.meals$?.subscribe();
+        this.meal$ = this.route.params.pipe(
+            switchMap(param => {
+                return this.mealService.getMeal(param['id']);
+            })
+        );
+    }
+    ngOnDestroy() {
+        this.subscription?.unsubscribe();
+    }
 }
--- final-project-angular-pro/src/health/shared/services/meals.service.ts
+++ final-project-angular-pro/src/health/shared/services/meals.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnInit } from "@angular/core";
 import { AngularFireDatabase, SnapshotAction } from "@angular/fire/compat/database";
-import { map, Observable, Subject, switchMap, tap} from "rxjs";
+import { filter, map, Observable, of, Subject, switchMap, tap} from "rxjs";
 import { AuthService } from "src/auth/shared/services/auth/auth.service";
 import { Store } from "src/store";
 
@@ -41,8 +41,20 @@ export class MealsService {
         return this.authService.user?.uid
     }
 
+    getMeal(key: string){
+        if(!key) return of({} as Meal);
+        return this.store.select<Meal[]>('meals').pipe(
+            filter(Boolean),
+            map((meals: Meal[]) =>  meals.find((meal: Meal) => meal.$key === key))
+        )
+    }
+
     addMeal(meal: Meal){
         return this.db.list(`meals/${this.uid}`).push(meal);
+    }
+
+    updateMeal(key: string ,meal: Meal){
+        return this.db.object(`meals/${this.uid}/${key}`).update(meal);
     }
 
     removeMeal(key: string){
